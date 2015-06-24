1a33a3dabb1b81b0b8d2d59df9ef41572dfb56b0
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2015-06-24 19:33:38 -0400

    Split vlbi_helpers in separate files.

---
+++ scintellometry/io/vlbi_helpers/utils.py
@@ -0,0 +1,54 @@
+import warnings
+
+__all__ = ['bcd_decode', 'bcd_encode', 'get_frame_rate']
+
+
+def bcd_decode(value):
+    bcd = value
+    result = 0
+    factor = 1
+    while bcd > 0:
+        digit = bcd & 0xf
+        if not (0 <= digit <= 9):
+            raise ValueError("Invalid BCD encoded value {0}={1}."
+                             .format(value, hex(value)))
+        result += digit * factor
+        factor *= 10
+        bcd >>= 4
+    return result
+
+
+def bcd_encode(value):
+    result = 0
+    factor = 1
+    while value > 0:
+        value, digit = divmod(value, 10)
+        result += digit*factor
+        factor *= 16
+    return result
+
+
+def get_frame_rate(fh, header_class, thread_id=None):
+    """Returns the number of frames
+
+    Can be for a specific thread_id (by default just the first thread in
+    the first header).
+    """
+    fh.seek(0)
+    header = header_class.fromfile(fh)
+    assert header['frame_nr'] == 0
+    sec0 = header.seconds
+    if thread_id is None and 'thread_id' in header:
+        thread_id = header['thread_id']
+    while header['frame_nr'] == 0:
+        fh.seek(header.payloadsize, 1)
+        header = header_class.fromfile(fh)
+    while header['frame_nr'] > 0:
+        max_frame = header['frame_nr']
+        fh.seek(header.payloadsize, 1)
+        header = header_class.fromfile(fh)
+
+    if header.seconds != sec0 + 1:
+        warnings.warn("Header time changed by more than 1 second?")
+
+    return max_frame + 1
