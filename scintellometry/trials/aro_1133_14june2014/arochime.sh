746a8fd7050ba17f2536c93a02309f4c021be7a4
Franz Kirsten <franzk@gpc-f104n084-ib0.(none)>
2017-04-24 00:44:18 -0400

    cleaned up submission script a little

--- scintellometry/trials/aro_1133_14june2014/arochime.sh
+++ scintellometry/trials/aro_1133_14june2014/arochime.sh
@@ -3,19 +3,9 @@
 #PBS -N fold_2048_secs
 #PBS -m abe
 
-## Note that the total number of mpi processes in a runjob (i.e., the --np argument) should be the ranks-per-node times the number of nodes (set by bg_size in the loadleveler script). So for the same number of nodes, if you change ranks-per-node by a factor of two, you should also multiply the total number of mpi processes by two.
-## One would therefore ideally use 64 / $OMP = 16 ranks per node
 NP=256
 OMP=4 # number of threads per node (4 available)
-RPN=4 # does not exist in intel mpirunRPN = 8 does not give memory errors for LOFAR, but 16 seems problematic
-# load modules (must match modules used for compilation)
-#module unload mpich2/xl
-#module load   python/2.7.3         binutils/2.23      bgqgcc/4.8.1       mpich2/gcc-4.8.1 fftw/3.3.3-gcc4.8.1 
-#module load xlf/14.1 essl/5.1
-#module load vacpp
-#module load hdf5/189-v18-mpich2-xlc
-#module load binutils/2.23 bgqgcc/4.8.1 mpich2/gcc-4.8.1 hdf5/1814-v18-mpich2-gcc python/2.7.3 
-#module load fftw/3.3.4-gcc4.8.1 
+
 module purge
 module load gcc/4.8.1 binutils 
 module load intel/16.0.3 python/2.7.8
